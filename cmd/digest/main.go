8e82fcefb6b5e8764f7394d5ac7125cd22ac8564
it512 <it512@msn.com>
2025-04-05 01:02:07 +0800

    flag

--- cmd/digest/main.go
+++ cmd/digest/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 
-	"github.com/icholy/digest"
 	"github.com/twiglab/doggy/holo"
 	"resty.dev/v3"
 )
@@ -13,18 +14,53 @@ type X struct {
 	PeerUrl string
 }
 
+func NewX(url string) *X {
+	return &X{Enable: 1, PeerUrl: url}
+}
+
+var (
+	addr string
+	port int
+
+	url string
+
+	password string
+	username string
+
+	ssl int
+)
+
+func postUrl() string {
+	if ssl == 0 {
+		return fmt.Sprintf("http://%s:%d/SDCAPI/V1.0/Rest/RestClient", addr, port)
+	}
+	return fmt.Sprintf("https://%s:%d/SDCAPI/V1.0/Rest/RestClient", addr, port)
+}
+
+func init() {
+	flag.IntVar(&port, "port", 80, "摄像头端口")
+	flag.StringVar(&addr, "addr", "", "摄像头地址")
+
+	flag.StringVar(&url, "url", "", "平台注册url")
+
+	flag.StringVar(&username, "username", "", "用户名")
+	flag.StringVar(&password, "password", "", "密码")
+
+	flag.IntVar(&ssl, "ssl", 1, "是否使用ssl(0不启用)")
+}
+
 func main() {
+	flag.Parse()
+
 	var cr holo.CommonResponse
 
-	c := resty.New()
-	c.SetTransport(&digest.Transport{
-		Username: "",
-		Password: "",
-	})
+	c := resty.New().SetDigestAuth(username, password)
+	defer c.Close()
 
-	_, err := c.R().SetBody(&X{Enable: 1, PeerUrl: ""}).
+	_, err := c.R().SetBody(NewX(url)).
 		SetResult(&cr).
-		Post("")
+		SetError(&cr).
+		Post(postUrl())
 
 	if err != nil {
 		log.Fatal(err)
