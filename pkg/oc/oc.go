bda93081ef13a4001c3070ebd4dc3a092b77b64f
it512 <it512@msn.com>
2025-05-19 07:24:43 +0800

    jsonrpc2

--- pkg/oc/oc.go
+++ pkg/oc/oc.go
@@ -1,11 +1,5 @@
 package oc
 
-import (
-	"context"
-
-	"github.com/imroc/req/v3"
-)
-
 type SumArgs struct {
 	Table string   `json:"table omitempty"`
 	Start int64    `json:"starte"`
@@ -16,37 +10,4 @@ type SumArgs struct {
 
 type SumReply struct {
 	Total int64 `json:"total"`
-}
-
-func ocURL(addr string) string {
-	url := "https://" + addr + "/out"
-	return url
-}
-
-type Client struct {
-	baseURL string
-	client  *req.Client
-}
-
-func New(addr string) *Client {
-	base := ocURL(addr)
-
-	c := req.C().
-		EnableInsecureSkipVerify().
-		SetCommonRetryCount(3).
-		SetBaseURL(base)
-
-	return &Client{
-		client:  c,
-		baseURL: base,
-	}
-}
-
-func (c *Client) Sum(ctx context.Context, args *SumArgs) (reply *SumReply, err error) {
-	_, err = c.client.R().
-		SetContext(ctx).
-		SetBody(args).
-		SetSuccessResult(&reply).
-		Post("/sum")
-	return
 }
