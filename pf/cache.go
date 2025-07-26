2beb0dbd8cba9116952e115d2de362c71b112350
it512 <it512@msn.com>
2025-07-26 09:22:29 +0800

    cache

--- pf/cache.go
+++ pf/cache.go
@@ -5,52 +5,65 @@ import (
 	"sync"
 )
 
-type Cache[K, V any] interface {
+type Cache[K comparable, V any] interface {
 	Get(context.Context, K) (V, bool, error)
 	Set(context.Context, K, V) error
 }
 
-type emptyCache[K, V any] struct{}
+type localCache[K comparable, V any] struct {
+	local sync.Map
+}
 
-func (e emptyCache[K, T]) Get(_ context.Context, _ K) (val T, ok bool, err error) { return }
-func (e emptyCache[K, T]) Set(_ context.Context, _ K, _ T) (err error)            { return }
+func (l *localCache[K, V]) Get(_ context.Context, k K) (v V, ok bool, err error) {
+	var a any
+	if a, ok = l.local.Load(k); ok {
+		v = a.(V)
+	}
+	return
+}
 
-type TiersCache[K, V any] struct {
-	m      sync.Map
+func (l *localCache[K, V]) Set(_ context.Context, k K, v V) error {
+	l.local.Store(k, v)
+	return nil
+}
+
+type emptyCache[K comparable, V any] struct{}
+
+func (e emptyCache[K, V]) Get(_ context.Context, _ K) (val V, ok bool, err error) { return }
+func (e emptyCache[K, V]) Set(_ context.Context, _ K, _ V) (err error)            { return }
+
+type TiersCache[K comparable, V any] struct {
+	local  Cache[K, V]
 	second Cache[K, V]
 }
 
-func NewTiersCache[K any, V any]() *TiersCache[K, V] {
-	return NewWithSecond(emptyCache[K, V]{})
+func NewTiersCache[K comparable, V any]() *TiersCache[K, V] {
+	return &TiersCache[K, V]{
+		local:  &localCache[K, V]{},
+		second: emptyCache[K, V]{},
+	}
 }
 
-func NewWithSecond[K any, V any](second Cache[K, V]) *TiersCache[K, V] {
-	return &TiersCache[K, V]{
-		second: second,
-	}
+func (t *TiersCache[K, V]) WithLocal(local Cache[K, V]) *TiersCache[K, V] {
+	t.local = local
+	return t
+}
+
+func (t *TiersCache[K, V]) WithSecond(second Cache[K, V]) *TiersCache[K, V] {
+	t.second = second
+	return t
 }
 
 func (t *TiersCache[K, V]) Get(ctx context.Context, key K) (v V, ok bool, err error) {
-	if v, ok, err = t.innerG(key); ok {
+	if v, ok, err = t.local.Get(ctx, key); ok {
 		return
 	}
 	if v, ok, err = t.second.Get(ctx, key); ok {
-		t.m.Store(key, v)
+		err = t.local.Set(ctx, key, v)
 	}
 	return
 }
 
 func (t *TiersCache[K, V]) Set(ctx context.Context, k K, v V) error {
-	t.m.Store(k, v)
-	return nil
-}
-
-func (t *TiersCache[K, V]) innerG(key K) (V, bool, error) {
-	a, ok := t.m.Load(key)
-	if ok {
-		return a.(V), true, nil
-	}
-
-	var v V
-	return v, false, nil
+	return t.local.Set(ctx, k, v)
 }
