9fbb37c2dfe8a8b563f1ea6554a2a565c6e30f1d
it512 <it512@msn.com>
2025-08-13 13:50:49 +0800

    x

--- pf/cloud.go
+++ pf/cloud.go
@@ -2,8 +2,11 @@ package pf
 
 import (
 	"context"
+	"log/slog"
+	"net/http"
 
 	"github.com/twiglab/doggy/holo"
+	"github.com/twiglab/doggy/hx"
 	"resty.dev/v3"
 )
 
@@ -47,4 +50,33 @@ func (c *CloudAction) HandleData(ctx context.Context, data UploadeData) error {
 		SetBody(upload)
 	_, err := r.Post("/cloud")
 	return err
+}
+
+type CloudHandle struct {
+	dataHandler DataHandler
+}
+
+func CloudUpload(h *CloudHandle) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var data UploadeData
+		if err := hx.BindAndClose(r, &data); err != nil {
+			slog.ErrorContext(r.Context(), "error-01",
+				slog.String("method", "MetadataEntryUpload"),
+				slog.Any("error", err))
+
+			http.Error(w, "error-01", http.StatusInternalServerError)
+			return
+		}
+
+		if err := h.dataHandler.HandleData(r.Context(), data); err != nil {
+			slog.ErrorContext(r.Context(), "error-02",
+				slog.String("method", "MetadataEntryUpload"),
+				slog.Any("error", err))
+
+			http.Error(w, "error-02", http.StatusInternalServerError)
+			return
+		}
+
+		hx.NoContent(w)
+	}
 }
