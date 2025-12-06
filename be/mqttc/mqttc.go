366185bd2f56340aedf42c8a3f060e93a6f9a5a1
it512 <it512@msn.com>
2025-12-06 17:15:04 +0800

    topic

--- be/mqttc/mqttc.go
+++ be/mqttc/mqttc.go
@@ -14,8 +14,8 @@ type MQTTAction struct {
 	client mqtt.Client
 }
 
-func New() *MQTTAction {
-	return &MQTTAction{}
+func New(client mqtt.Client) *MQTTAction {
+	return &MQTTAction{client: client}
 }
 
 func (c *MQTTAction) HandleData(ctx context.Context, data human.DataMix) error {
@@ -27,7 +27,11 @@ func (c *MQTTAction) HandleData(ctx context.Context, data human.DataMix) error {
 		return err
 	}
 
-	token := c.client.Publish("test/topic", 0, false, bb)
+	token := c.client.Publish(pushTopic(data.Head.UUID, data.Type), 0, false, bb)
 
 	return token.Error()
+}
+
+func pushTopic(uuid, typ string) string {
+	return "dcp/" + uuid + "/" + typ
 }
