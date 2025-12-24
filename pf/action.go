9929aa99485b44542b84d546189910cdb04e19b3
it512 <it512@msn.com>
2025-12-24 12:55:50 +0800

    log

--- pf/action.go
+++ pf/action.go
@@ -8,13 +8,13 @@ import (
 	"github.com/twiglab/doggy/pkg/human"
 )
 
-type noopAction struct{}
+type LogAction struct{}
 
-func (d noopAction) Name() string {
-	return "noop"
+func (d LogAction) Name() string {
+	return "log"
 }
 
-func (d noopAction) AutoRegister(ctx context.Context, data holo.DeviceAutoRegisterData) error {
+func (d LogAction) AutoRegister(ctx context.Context, data holo.DeviceAutoRegisterData) error {
 	slog.DebugContext(ctx, "receive reg data",
 		slog.String("module", "cameraAction"),
 		slog.String("sn", data.SerialNumber),
@@ -22,7 +22,7 @@ func (d noopAction) AutoRegister(ctx context.Context, data holo.DeviceAutoRegist
 	return nil
 }
 
-func (d noopAction) HandleData(ctx context.Context, data human.DataMix) error {
+func (d LogAction) HandleData(ctx context.Context, data human.DataMix) error {
 	slog.DebugContext(ctx, "handleData", slog.Any("data", data))
 	return nil
 }
